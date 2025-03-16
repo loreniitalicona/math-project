f7619b57fa675bd7b6b155a4c0d466162eb53c18
loreniitalicona <loreniitalicona@gmail.com>
2025-03-16 15:05:21 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x = 2.345;
+    double y = 1.234;
+    double z = (x + y) / 2;
+    cout << "The average of " << x << " and " << y << " is " << z << endl;
+    return 0;
+}
