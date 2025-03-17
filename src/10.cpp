e0d5b06741adef570dc09a66dd737e98600afb1e
loreniitalicona <loreniitalicona@gmail.com>
2025-03-17 16:24:01 +0000

    Add 10.cpp

---
+++ src/10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x = 5.0;
+    double y = 3.0;
+    double z = pow(x, y);
+    cout << "The value of z is: " << z << endl;
+    return 0;
+}
