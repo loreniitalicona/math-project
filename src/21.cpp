3cd5da43d7db713266d6f169ea510c847aa4d13d
loreniitalicona <loreniitalicona@gmail.com>
2025-04-01 15:54:54 +0000

    Add 21.cpp

---
+++ src/21.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = a / b; // Perform division and print result
+    cout << "The result of a/b is: " << c << endl;
+    return 0;
+}
