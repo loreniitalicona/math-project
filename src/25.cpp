67ad28b1a0c7e2ef0becd363ca0c3c7bed5630c6
loreniitalicona <loreniitalicona@gmail.com>
2025-04-07 14:43:42 +0000

    Add 25.cpp

---
+++ src/25.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        cout << "X is greater than Y" << endl;
+    } else {
+        cout << "Y is greater than X" << endl;
+    }
+}
