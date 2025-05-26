2602205eecf3db8738ad84f8627b1a2c8674caab
loreniitalicona <loreniitalicona@gmail.com>
2025-05-26 09:02:15 +0000

    Add 41.cpp

---
+++ src/41.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    double z = 2.5;
+    float f = 10.2f;
+
+    cout << "x is " << x << endl;
+    cout << "y is " << y << endl;
+    cout << "z is " << z << endl;
+    cout << "f is " << f << endl;
+
+    return 0;
+}
